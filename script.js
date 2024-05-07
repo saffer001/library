57e043d06f1feaa26c9222791a722d90a1a2abd6
saffer001 <21124395+saffer001@users.noreply.github.com>
2024-05-07 22:00:18 +0200

    add functionality to add books and remove them from the list

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ class Book {
     }
 };
 
-
-
 let threeBodyProblem = new Book("Three Body Problem","Cixin Liu","2008", false );
 let fellowshipOfTheRing = new Book("The Fellowship of the Ring","J. R. R. Tolkien","1954", false );
 let mobyDick = new Book("Moby Dick","Herman Melville","1851", false );
 
 const arrayBooks = [threeBodyProblem, fellowshipOfTheRing, mobyDick];
 
-// Static DOM Elements
-const addBookButton = document.getElementById("addBook");
+// Static DOM Elements --------------
 const cardContainer = document.getElementById("book-cards");
-const updateButton = document.getElementById("updateDetails");
-const cancelButton = document.getElementById("cancel");
 const dialog = document.getElementById("favDialog");
+//select
 const selectReleaseDate = document.getElementById("releaseDate");
+// input fields
+const inputBookTitle = document.getElementById("bookTitle");
+const inputBookAuthor = document.getElementById("bookAuthor");
+//buttons
+const submitButton = document.getElementById("submitButton");
+const cancelButton = document.getElementById("cancel");
+const updateButton = document.getElementById("updateDetails");
+const addBookButton = document.getElementById("addBook");
+
+//populate with a few static items
+addNewBook("The Three Body Problem", "Cixin Liu", 2008);
+addNewBook("The Fellowship of the Ring", "J. R. R. Tolkien", 1954);
+addNewBook("Moby Dick", "Herman Melville", 1851);
 
 //populating release date selector
 for (let index = 2000; index >= 1000; index--) {
@@ -31,17 +40,6 @@ for (let index = 2000; index >= 1000; index--) {
   selectReleaseDate.appendChild(optionReleaseDate);
   
 }
-//populating book list with static elements
-arrayBooks.forEach((currentBook) => {
-    let newDiv = document.createElement("div");
-    newDiv.textContent = `Title: ${currentBook.title}\r\nAuthor: ${currentBook.author}\r\nRelease date: ${currentBook.releaseDate}` ;
-    let delButton = document.createElement("button");
-    delButton.textContent = "Remove";
-    delButton.className = "button-delete";
-    cardContainer.appendChild(newDiv);
-    newDiv.appendChild(delButton);
-});
-
   
   // Update button opens a modal dialog
   addBookButton.addEventListener("click", () => {
@@ -52,3 +50,31 @@ arrayBooks.forEach((currentBook) => {
   cancelButton.addEventListener("click", () => {
     dialog.close();
   });
+
+submitButton.addEventListener("click", () => {
+  addNewBook(inputBookTitle.value, inputBookAuthor.value, selectReleaseDate.value);
+});
+
+function addNewBook(title, author, date) {
+  let addNewBook = new Book(title, author, date);
+  arrayBooks.push(addNewBook);
+
+  let bookContainer = document.createElement("div");
+  bookContainer.className = "bookContainer";
+
+  let newBookDiv = document.createElement("div");
+  newBookDiv.innerHTML = `Title: ${addNewBook.title}<br>Author: ${addNewBook.author}<br>Release date: ${addNewBook.releaseDate}`;
+  newBookDiv.className = "newBookDiv";
+
+  const removeButton = document.createElement("button");
+  removeButton.className = "removeButton";
+  removeButton.textContent = "Remove";
+  removeButton.onclick = function()  {
+    cardContainer.removeChild(bookContainer);
+  };
+  bookContainer.appendChild(newBookDiv);
+  bookContainer.appendChild(removeButton);
+  cardContainer.appendChild(bookContainer);  
+
+};
+
